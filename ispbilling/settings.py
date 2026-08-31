cd6a35eaa0d5fed09cf74d030e7028689c6b006c
haru-voster <wabwireharoun2001@gmail.com>
2026-08-31 16:00:43 +0300

    voster

--- ispbilling/settings.py
+++ ispbilling/settings.py
@@ -19,13 +19,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING: keep the secret key used in production secret!,
 SECRET_KEY = 'django-insecure-+xefv2x1pf374u8@+4+9(=po1a5dl5_^re7ucw+!hu!c)bcy%^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["isp.haruvoster.co.ke", "www.isp.haruvoster.co.ke"]
+ALLOWED_HOSTS = ["isp.haruvoster.co.ke", "www.isp.haruvoster.co.ke", '127.0.0.1', 'localhost']
 
 CSRF_TRUSTED_ORIGINS = ["https://isp.haruvoster.co.ke", "https://www.isp.haruvoster.co.ke"]
 
